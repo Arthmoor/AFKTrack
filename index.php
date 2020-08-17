6fd237bdba716166e126eef2717e60f6ffa88998
Arthmoor <arthmoor@gmail.com>
2020-08-17 14:52:45 -0600

    Update to enforce PHP 7.4 required.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Based on the Sandbox package: https://github.com/Arthmoor/Sandbox
  */
 
-if( version_compare( PHP_VERSION, "7.3.0", "<" ) ) {
+if( version_compare( PHP_VERSION, "7.4.0", "<" ) ) {
 	die( 'PHP version does not meet minimum requirements. Contact your system administrator.' );
 }
 
